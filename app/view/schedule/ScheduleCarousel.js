524917a442219ba03950dffde968a476be5438c7
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-06-22 17:46:30 +0200

    carousel item width

--- app/view/schedule/ScheduleCarousel.js
+++ app/view/schedule/ScheduleCarousel.js
@@ -8,21 +8,37 @@ Ext.define("studiplaner.view.schedule.ScheduleCarousel", {
     config: {
 		fullscreen: true,
 		direction: 'horizontal',
-		itemLength: 250,
+		itemLength: window.innerWidth-60,
 		defaults: {
 			styleHtmlContent: true
 		},
 		items: [{
-				html : 'Item 1',
+				html : 'Montag',
 				style: 'background-color: #759E60'
 			},
 			{
-				html : 'Item 2',
+				html : 'Dienstag',
 				style: 'background-color: #5E99CC'
 			},
 			{
-				html : 'Item 3',
-				style: 'background-color: #00000F'
+				html : 'Mittwoch',
+				style: 'background-color: #702097'
+			},
+			{
+				html : 'Donnerstag',
+				style: 'background-color: #5454C9'
+			},
+			{
+				html : 'Freitag',
+				style: 'background-color: #35B795'
+			},
+			{
+				html : 'Samstag',
+				style: 'background-color: #DE21C9'
+			},
+			{
+				html : 'Sonntag',
+				style: 'background-color: #D7C941'
 			}
 		]
     }
