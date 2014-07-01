4bffcfd01a0ecb92330be71f910b6438b1acf539
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-07-01 14:15:33 +0200

    set grouped to false

--- app/view/modules/ModulesList.js
+++ app/view/modules/ModulesList.js
@@ -9,7 +9,7 @@ Ext.define("studiplaner.view.modules.ModulesList", {
         loadingText: "Lade Module...",
         emptyText: '</pre> <div class="notes-list-empty-text">Aktuell liegen keine Module vor.</div> <pre>',
         onItemDisclosure:true,
-        grouped: true,
+        grouped: false,
         itemTpl: 	'</pre><div id="{id}"><span class="list-item-name">{name}</span>'+
 					'<span class="list-item-workload">{workload} Std./Woche</span><div><pre>'
     }
--- app/view/work/WorkList.js
+++ app/view/work/WorkList.js
@@ -9,7 +9,7 @@ Ext.define("studiplaner.view.work.WorkList", {
         loadingText: "Lade Arbeitszeiten...",
         emptyText: '</pre><div class="notes-list-empty-text">Aktuell liegen keine Arbeitszeiten vor.</div> <pre>',
         onItemDisclosure:true,
-        grouped: true,
+        grouped: false,
         itemTpl: 	'</pre><div id="{id}"><span class="list-item-name">{name}</span>'+
 					'<span class="list-item-workload">{workload} Std./Woche</span><div><pre>'
     }
