21173da638474aa6e283f894bb42b62771bc10a4
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-07-06 21:46:02 +0200

    disabled grouped

--- app/view/workload/WorkloadWorkList.js
+++ app/view/workload/WorkloadWorkList.js
@@ -9,7 +9,7 @@ Ext.define("studiplaner.view.workload.WorkloadWorkList", {
         loadingText: "Lade Arbeitszeiten...",
         emptyText: '</pre><div class="notes-list-empty-text">Aktuell liegen keine Arbeitszeiten vor.</div> <pre>',
         onItemDisclosure:true,
-        grouped: true,
+        grouped: false,
         itemTpl: 	'</pre><div id="{id}"><span class="list-item-name">{name}</span>'+
 					'<span class="list-item-workload list-item-workload-nopad">{workload} Std./Woche</span><div><pre>'
     }
