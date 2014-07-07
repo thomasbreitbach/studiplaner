9e0769aaf032d681e4cb1612e2342f5a9b97cfd7
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-07-07 12:13:37 +0200

    list template

--- app/view/schedule/BlocksList.js
+++ app/view/schedule/BlocksList.js
@@ -11,7 +11,15 @@ Ext.define("studiplaner.view.schedule.BlocksList", {
         emptyText: '</pre> <div class="notes-list-empty-text">Bevor der Wocheplan angelegt werden kann, musst du zuerst in der Modulverwaltung deine Module eintragen.</div> <pre>',
         
         
-        itemTpl: 	'</pre><div id="{id}"><span class="list-item-name">{Module.name}</span>'+
-					'<span class="list-item-workload">zugew.: {assigned}</span><div><pre>'
+        itemTpl: 	'</pre>'+
+						'<div id="{id}">'+
+							'<span >{Module.name}</span>'+
+							'<span class="blocklist-assigned">zugew.: {assigned}</span>'+
+						'</div>'+
+						'<div class="blocklist-row-2">'+
+							'<span >{type}</span>'+
+							'<span class="blocklist-assigendTo">{day} - {block}</span>'+
+						'</div>'+
+					'<pre>'
     }
 });
