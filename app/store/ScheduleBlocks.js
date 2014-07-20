4222bebf304defaacef2d7b827bf58038cfc5ff0
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-07-20 19:02:28 +0200

    sort schedule blocks by module name

--- app/store/ScheduleBlocks.js
+++ app/store/ScheduleBlocks.js
@@ -17,18 +17,29 @@ Ext.define("studiplaner.store.ScheduleBlocks", {
             sortProperty: "type",
             direction: "ASC",
             groupFn: function (record) {
-				//~ console.log(record.ModuleBelongsToInstance);
                 if (record && record.data.type) {
 					if(record.data.type === 'presence'){
 						return 'Anwesenheit';
 					}else{
 						return 'Selbststudium';
 					}
-                } else {
-                    return '';
                 }
             }
         },
-        sorters: [{ property: 'Model.name', direction: 'ASC'}],
+        sorters: [
+			{
+				// Sort by first letter of last name, in descending order
+				sorterFn: function(record1, record2) {
+					var modulesStore = Ext.getStore('Modules'),
+						mod1 = modulesStore.getById(record1.data.module_id),
+						mod2 = modulesStore.getById(record2.data.module_id),
+						name1 = mod1.data.name.substr(0, 1),
+						name2 = mod2.data.name.substr(0, 1);
+
+					return name1 > name2 ? 1 : (name1 === name2 ? 0 : -1);
+				},
+				direction: 'ASC'
+			}
+		],
     }
 });
