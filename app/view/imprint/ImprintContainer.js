534529a779930bc6c82d0e9da58163307c6b5569
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-07-28 15:41:46 +0200

    imprint

--- app/view/imprint/ImprintContainer.js
+++ app/view/imprint/ImprintContainer.js
@@ -12,43 +12,32 @@ Ext.define('studiplaner.view.imprint.ImprintContainer', {
   	config: {
 		layout: {
         	type: 'fit'
-    	}
-  	},
-
-  	initialize: function(){
-		this.callParent(arguments);
-	
-		var menuButton = {
-            xtype: "button",
-        	iconCls: 'list',
-        	ui: 'action',
-        	handler: this.onMenuButtonTap,
-        	scope: this
-		};
-
-		var topToolbar = {
-		    xtype: "toolbar",
-		    title: 'Impressum',
-		    docked: "top",
-		    items: [
-				menuButton
-		    ]
-		};
-		
-		var content = {
+    	},
+    	scrollable:'vertical',
+    	items: [{
+			xtype: "toolbar",
+			title: 'Impressum',
+			docked: "top",
+			items: [{
+				xtype: "button",
+				iconCls: 'list',
+				ui: 'action',
+				handler: this.onMenuButtonTap,
+				scope: this
+			}]
+		}, {
 			xtype: "container",
 			cls: 'imprint-body',
 			html: 	'<div class="thm-logo"></div>'+
 					'<h1>Dies ist ein Produkt der THM - Technische Hochschule Mittelhessen</h1>'+
 					'<p>Wiesenstraße 14<br>35390 Gießen</p>'+
-					'<p>Planung und Realisierung:<br>Thomas Breitbach<p>'+
 					'<p>Modul:<br>Entwicklunsprojekt</p>'+
+					'<p>Betreuung:<br>Prof. Dr. rer. nat. Dominik Schultes</p>'+
+					'<p>Planung und Realisierung:<br>Thomas Breitbach<p>'+
+					'<p>Idee:<br>M. H. Edu. Sabine Langkamm<p>'+		
 					'<p>Source-Code:<br><a href="https://github.com/thomasbreitbach/studiplaner" title="Studiplaner Source-Code auf GitHub">Studiplaner auf GitHub</a></p>'
-		};
-
-  
-    	this.add([topToolbar, content]);
-	},
+		}]
+  	},
 	
 	onMenuButtonTap: function (){
 		this.fireEvent("toggleSlideMenuCommand", this);
