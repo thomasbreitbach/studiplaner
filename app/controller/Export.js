974b4ee0cb0ba59b5e665e7c6250ad52582ffa4a
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2014-08-08 21:04:19 +0200

    scheduling txt

--- app/controller/Export.js
+++ app/controller/Export.js
@@ -24,12 +24,44 @@ Ext.define("studiplaner.controller.Export", {
     slideRightTransition: { type: 'slide', direction: 'right' },
     
     //*************
+    //***HELPER****
+    //*************
+    buildSchedulingTxt: function (){
+		var txt = "";
+		var workload = 0;
+		var modulesStore = Ext.getStore("Modules");
+		var modules = modulesStore.getData().items;
+		
+		txt += "Module:\n"
+		for(var i = 0; i < modules.length; i ++){
+			var module = modules[i].data;
+			txt += module.name + ", " + module.workload + " Std./Woche\n";
+			workload += module.workload;
+		}	
+		
+		var worksStore = Ext.getStore("Works");
+		var works = worksStore.getData().items;
+		
+		txt += "\nArbeitsstellen:\n"
+		for(var i = 0; i < works.length; i ++){
+			var work = works[i].data;
+			txt += work.name + ", " + work.workload + " Std./Woche\n";
+			workload += work.workload;
+		}
+		
+		txt += "\nWorkload gesamt:\n" + workload + " Std./Woche";
+
+		return txt;
+	},
+    
+    //*************
     //**COMMANDS***
     //*************
     onGenerateMail: function () {
 		console.log("onShowExportForm");
+		var schedulingTxt = this.buildSchedulingTxt();
 		var newMail = Ext.create("studiplaner.model.ExportMail", {
-			scheduling: "Balbalbal"
+			scheduling: schedulingTxt
 	    });
 	    
 	    this.getExportForm().setRecord(newMail);
